fe7eab3a4757acc2bc90c6d1acae01c9be9e97e7
amccaskey9 <114969372+amccaskey9@users.noreply.github.com>
2022-12-19 23:40:06 -0600

    Added Current Time
    Added current time to API and removed console log

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-let apiKey = "f08a6a7fd3e944f30od1c4cc4b5b3tf6";
-let apiEndpoint = "https://api.shecodes.io/weather/v1/";
-let units = "metric";
-let apiUrl = `${apiEndpoint}current?query=Houston&key=${apiKey}&units=${units}`;
-apiUrl = apiUrl.replace(" ", "+");
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 
 function showCurrent(response) {
-  console.log(response.data);
-
   let cityName = document.querySelector("#city");
   cityName.innerHTML = response.data.city;
 
@@ -21,6 +36,15 @@ function showCurrent(response) {
 
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = response.data.temperature.humidity;
+
+  let date = document.querySelector("#date");
+  date.innerHTML = formatDate(response.data.time * 1000);
 }
+
+let apiKey = "f08a6a7fd3e944f30od1c4cc4b5b3tf6";
+let apiEndpoint = "https://api.shecodes.io/weather/v1/";
+let units = "metric";
+let apiUrl = `${apiEndpoint}current?query=Houston&key=${apiKey}&units=${units}`;
+apiUrl = apiUrl.replace(" ", "+");
 
 axios.get(apiUrl).then(showCurrent);
